4da0dcb3499c03e41f1081115c08fe87825fe9fb
Poojagit10 <115066241+Poojagit10@users.noreply.github.com>
2022-10-05 12:58:41 +0530

    Add files via upload

---
+++ pickling.py
@@ -0,0 +1,15 @@
+#import pickle
+#a = ['test value','test value 2','test value 3']
+#file_Name = "Marks"
+#fileObject = open(file_Name,'wb')
+#pickle.dump(a,fileObject)
+#fileObject.close()
+##
+### we open the file for reading
+#fileObject = open(file_Name,'r')
+#b = pickle.load(fileObject)
+#print(b)
+
+import calendar
+
+print(calendar.isleap(2020))
